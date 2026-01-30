a5da09f0d8f73d8a9929b261cd3c6dcff377eafe
Leif Arriens <leif.arriens@gmail.com>
2026-01-30 02:09:23 +0100

    chore: update tsconfigs

--- packages/hooks/src/useMeasure/useMeasure.test.tsx
+++ packages/hooks/src/useMeasure/useMeasure.test.tsx
@@ -1,5 +1,4 @@
 import { cleanup, render, waitFor } from "@testing-library/react";
-import React from "react";
 import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 
 import { useMeasure } from "./useMeasure.js";
