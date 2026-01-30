1aede7515e763c88936992eb1736736608f4e543
Leif Arriens <leif.arriens@gmail.com>
2026-01-30 02:09:23 +0100

    chore: update tsconfigs

--- packages/hooks/src/useHover/useHover.test.tsx
+++ packages/hooks/src/useHover/useHover.test.tsx
@@ -1,5 +1,4 @@
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
-import React from "react";
 import { afterEach, describe, expect, it } from "vitest";
 
 import { useHover } from "./useHover.js";
